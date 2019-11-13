04ff58785d4060df73227a7a7bb4c4c094a18db5
Aleksandr Grinchenko <sasha_grinchenko@outlook.com>
2019-11-13 19:27:45 +0200

    test

--- LukashynBinaryTree/BinaryTreeElement.cs
+++ LukashynBinaryTree/BinaryTreeElement.cs
@@ -5,7 +5,13 @@ namespace LukashynBinaryTree
     public class BinaryTreeElement<T> where T : IComparable
     {
         public T Value { get; set; }
+        /// <summary>
+        /// A reference to a child node whose value is less than the parent
+        /// </summary>
         public BinaryTreeElement<T> PrevElement { get; internal set; }
+        /// <summary>
+        /// A reference to a child node whose value is equal to or greater than the parent
+        /// </summary>
         public BinaryTreeElement<T> NextElement { get; internal set; }
 
         public BinaryTreeElement(T input)
